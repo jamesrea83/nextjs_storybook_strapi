b170de6c3373784dcb3b256f8e23de279f429065
jamesrea83 <james@stream.bid>
2023-05-11 16:42:56 +0100

    react-hook-form setup

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
-import Link from 'next/link';
+import { forwardRef, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import styled from '@emotion/styled';
 
 import { CenteredTile } from '@/components/Tile';
@@ -11,14 +12,39 @@ const StyledInput = styled(Input)`
 	margin-bottom: 1rem;
 `;
 
+export type LoginForm = {
+	identifier: string;
+	password: string;
+};
+
 const Login: NextPage = () => {
-	const onSubmit = () => null;
+	const { register, handleSubmit, setValue, setError } = useForm<LoginForm>();
+	const onSubmit = (data: LoginForm) => {
+		console.log(data);
+	};
+
+	useEffect(() => {
+		register('identifier', { required: true, minLength: 6 });
+		register('password', { required: true, minLength: 6 });
+	}, []);
 
 	return (
-		<form onSubmit={onSubmit}>
+		<form onSubmit={handleSubmit(onSubmit)}>
 			<CenteredTile header="Login">
-				<StyledInput label="Identifier" placeholder="username or email" />
-				<StyledInput label="Password" placeholder="password" />
+				<StyledInput
+					name="identifier"
+					onChange={event => setValue('identifier', event.target.value)}
+					label="Identifier"
+					placeholder="username or email"
+					height={6}
+				/>
+				<StyledInput
+					name="password"
+					onChange={event => setValue('password', event.target.value)}
+					label="Password"
+					placeholder="password"
+					height={6}
+				/>
 				<Button type="submit">Sign in</Button>
 				<h3>
 					<StyledLink href="/registration" underline={1}>
