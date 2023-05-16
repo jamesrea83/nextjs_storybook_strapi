d0f1132be4af043493f2b6679ec19768c869aeb3
jamesrea83 <james@stream.bid>
2023-05-16 16:36:44 +0100

    msw installed

---
+++ frontend/mocks/handlers.ts
@@ -0,0 +1,30 @@
+import { rest } from 'msw';
+
+import { LoginData } from '@/services/userSlice';
+import { mockUser } from '@/mocks/user';
+
+const api_url = process.env.NEXT_PUBLIC_STRAPI_API_URL;
+
+const ValidationError = {
+	error: {
+		status: 400,
+		name: 'ValidationError',
+		message: 'Invalid identifier or password',
+		details: {},
+	},
+};
+
+export const handlers = [
+	rest.post<LoginData>(`${api_url}/auth/local`, async (req, res, ctx) => {
+		const { identifier, password } = await req.json();
+
+		if (
+			identifier === mockUser.user.email &&
+			password === mockUser.user.password
+		) {
+			return res(ctx.status(200), ctx.json(mockUser));
+		}
+
+		return res(ctx.status(400), ctx.json(ValidationError));
+	}),
+];
